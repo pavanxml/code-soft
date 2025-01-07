baee827862b10eee5be5261711873f6e4dcb5313
pavanxml <155630116+pavanxml@users.noreply.github.com>
2025-01-07 13:12:28 +0530

    day-2

--- task1/Guessing.java
+++ task1/Guessing.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Guessing {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the minimum value: ");
-        int min = scanner.nextInt();
+        int min = sc.nextInt();
 
         System.out.print("Enter the maximum value: ");
-        int max = scanner.nextInt();
+        int max = sc.nextInt();
 
         int secretNumber = (int) (Math.random() * (max - min + 1) + min); 
         int guess;
@@ -18,7 +18,7 @@ public class Guessing {
 
         do {
             System.out.print("Enter your guess: ");
-            guess = scanner.nextInt();
+            guess = sc.nextInt();
 
             if (guess < secretNumber) {
                 System.out.println("Too low! Try again.");
